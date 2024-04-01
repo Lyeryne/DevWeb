0ceae47c4c27e7cc0307d35f4fcccbfaf2f3964b
lyeryne <lyeryne@gmail.com>
2024-04-01 14:50:36 +0200

    6th_commit

---
+++ TEST/index.js
@@ -0,0 +1,7 @@
+var obj = {
+    monAttribut: "valeur",
+    maMethode: function(p)
+    { console.log(alert("parametre : " + p));}
+    };
+    console.log(alert(obj.monAttribut));
+    obj.maMethode("test"); 
